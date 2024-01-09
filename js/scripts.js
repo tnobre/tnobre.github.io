e8d2f671acf194d829d65ea8856e3a2b78dd86f6
Tiago Nobre <t.nobre@bo1.de>
2024-01-09 19:12:36 -0300

    Black background / some translation

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/1ELlGz0QxddpIJH0h1SW2TwAvtwXx7jJk6GxTELtGXAk/exec', data)
+            $.post('https://script.google.com/macros/s/.../exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
